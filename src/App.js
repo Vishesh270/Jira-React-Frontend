5c4182f304ebef552c47e7429c6abfd07f763012
Vishesh <visheshvishwakarma2700@gmail.com>
2024-05-17 01:11:39 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Signup from './Components/Signup/Signup';
+import Login from './Components/login';
+
+function App() {
+  return (
+
+    <BrowserRouter>
+        <Login/>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
